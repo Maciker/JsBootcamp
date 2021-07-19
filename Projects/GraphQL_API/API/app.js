a550d3364221af1170330c2cb15aec20d54bab8f
MacIker <ikermacaya@gmail.com>
2021-07-19 19:53:46 +0200

    The Graphl API

---
+++ Projects/GraphQL_API/API/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema.js')
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}))
+
+app.listen(process.env.PORT, () => {
+  console.log('App listening on port 3000')
+})
